7e97a5046efaa1cfa2764ea1782e519678e34af2
tvoutsos <tvoutsos@chicagobooth.edu>
2020-11-16 04:48:08 +0000

    Added flash messages

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -16,9 +16,14 @@ class Movie < ApplicationRecord
 
   has_many(:characters, { :class_name => "Character", :foreign_key => "movie_id", :dependent => :destroy })
 
-  belongs_to(:director, { :required => false, :class_name => "Director", :foreign_key => "director_id" })
+  belongs_to(:director, { :required => true, :class_name => "Director", :foreign_key => "director_id" })
 
   has_many(:cast, { :through => :characters, :source => :actor })
 
-  
+  validates(:year, { :numericality => { :less_than_or_equal_to => 2030, :greater_than_or_equal_to => 1800} })
+
+  validates(:title, { :uniqueness => {:scope => ["director_id"]}})
+
+  validates(:title, { :presence => true })
+
 end
